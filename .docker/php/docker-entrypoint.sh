8d96dbd007a0ae012f092db8b712a66a991806bf
chris cnizzardini <cnizzardini@gmail.com>
2021-05-29 16:45:15 -0400

    test

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -10,11 +10,11 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/cakephp' ]; then
 
     chown -R cakephp:www-data .
 
-    if [ "$APP_ENV" = 'prod' ]; then
-        composer install --prefer-dist --no-interaction --no-dev
-    else
-        composer install --prefer-dist --no-interaction
-    fi
+    #if [ "$APP_ENV" = 'prod' ]; then
+    #    composer install --prefer-dist --no-interaction --no-dev
+    #else
+    #    composer install --prefer-dist --no-interaction
+    #fi
 
     mkdir -p logs tmp/cache/models tmp/cache/models/persistent tmp/sessions tmp/tests
     chown -R cakephp:www-data logs tmp
