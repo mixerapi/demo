117191535e7debb8fdbbcfa876734045881699c1
chris cnizzardini <cnizzardini@gmail.com>
2021-05-29 15:26:36 -0400

    debugging kube deploy

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -9,14 +9,16 @@ fi
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/cakephp' ]; then
 
     chown -R cakephp:www-data .
-    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX logs
-    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX tmp
 
     if [ "$APP_ENV" = 'prod' ]; then
         composer install --prefer-dist --no-interaction --no-dev
     else
         composer install --prefer-dist --no-interaction
     fi
+
+    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX logs
+    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX tmp
+    bin/cake cache clear_all
 
 fi
 
