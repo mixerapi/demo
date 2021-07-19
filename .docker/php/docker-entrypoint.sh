120a4326035d41dd787e7184b816c5a0e3de5bdd
Chris Nizzardini <cnizzardini@gmail.com>
2021-07-19 10:04:56 -0400

    Update docker-entrypoint.sh

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -31,6 +31,13 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/cakephp' ]; then
 
         composer require mixerapi/mixerapi
         bin/cake plugin load MixerApi
+        
+        composer require mixerapi/crud
+        bin/cake plugin load MixerApi/Crud
+        
+        composer require friendsofcake/search
+        bin/cake plugin load Search        
+        
         bin/cake mixerapi install --auto Y
     fi
 
